df011963ffa977b256b3e77f27f309e324281b44
wllmjoxx <71368729+wllmjoxx@users.noreply.github.com>
2020-10-06 22:55:59 +0700

    Add files via upload

---
+++ NestedLoop_Week3.java
@@ -0,0 +1,50 @@
+package com.pboreg;
+
+public class NestedLoop_Week3 {
+    public static void main (String[] args){
+        //nested loop atau loop bersarang
+        for(int i = 0; i < 5; i++){
+            for(int j = 0; j < 5; j++){
+                System.out.print("* ");
+            }
+            System.out.print("\n");
+        }
+        System.out.print("\n");
+        for(int i = 0; i < 5; i++){
+            for(int j = 0; j < 5; j++){
+                System.out.print("* ");
+                if(i == j){
+                    break;
+                }
+            }
+            System.out.print("\n");
+        }
+
+        System.out.print("\n");
+
+        for(int i = 0; i < 5; i++){
+            for(int j = 0; j < 5; j++){
+                System.out.print("* ");
+                if ( (i+j) == 4){
+                    break;
+                }
+            }
+            System.out.print("\n");
+        }
+
+        System.out.print("\n");
+
+        for(int i = 0; i < 9; i++){
+            for(int j = 0; j < 9; j++){
+                System.out.print("* ");
+                if (i == j){
+                    break;
+                } else if ( (i+j) == 8){
+                    break;
+                }
+            }
+
+            System.out.print("\n");
+        }
+    }
+}
